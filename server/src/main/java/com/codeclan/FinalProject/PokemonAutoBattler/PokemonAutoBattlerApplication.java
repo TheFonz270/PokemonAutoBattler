03f952766d11118248f83c7723217ba40937ebce
Martin Quinn <thefonz270@hotmail.com>
2022-01-31 12:54:50 +0000

    initil commit of react app client and spring app server

---
+++ server/src/main/java/com/codeclan/FinalProject/PokemonAutoBattler/PokemonAutoBattlerApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.FinalProject.PokemonAutoBattler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonAutoBattlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonAutoBattlerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/codeclan/FinalProject/PokemonAutoBattler/PokemonAutoBattlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.FinalProject.PokemonAutoBattler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonAutoBattlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
