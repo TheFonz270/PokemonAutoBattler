26be75c62c29a42504bec3f7b1b0d175ae8e7182
rogertmalg <roger.malgueira@outlook.com>
2022-02-07 10:04:55 +0000

    adds more requests

--- server/src/main/java/com/codeclan/FinalProject/PokemonAutoBattler/controllers/TrainerController.java
+++ server/src/main/java/com/codeclan/FinalProject/PokemonAutoBattler/controllers/TrainerController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TrainerController {
@@ -21,4 +23,10 @@ public class TrainerController {
     public ResponseEntity<List<Trainer>> getAllTrainers(){
         return new ResponseEntity<>(trainerRepository.findAll(), HttpStatus.OK);
     }
+
+    @GetMapping(value = "/trainers/{id}")
+    public ResponseEntity<Optional<Trainer>> getTrainer(@PathVariable Long id){
+        return new ResponseEntity<>(trainerRepository.findById(id), HttpStatus.OK);
+    }
+
 }
